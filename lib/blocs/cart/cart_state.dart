ac2aaa9c95e4483ad682c6b09b87aab49b67def0
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-07 02:35:38 -0400

    implementacion de los estados que va a tener la app, o sea la informacion que necesita para dibujar la UI

---
+++ lib/blocs/cart/cart_state.dart
@@ -0,0 +1,25 @@
+import 'package:app_cart/core/domain/cart_item.dart';
+//informacion que la UI necesita para dibujar
+
+abstract class CartState {}
+
+class CartEmpty extends CartState {}
+
+class CartLoading extends CartState {}
+
+class CartUpdated extends CartState {
+  final List<CartItem> items;
+
+  CartUpdated({required this.items});
+
+  int get totalItems => items.fold(0, (sum, item) => sum + item.quantity);
+
+  double get totalPrice =>
+      items.fold(0, (sum, item) => sum + item.product.price * item.quantity);
+}
+
+class CartError extends CartState {
+  final String message;
+
+  CartError({required this.message});
+}

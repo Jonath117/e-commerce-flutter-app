6af9ab7ca94cdc50993546b7c4196eb73dd4c0a1
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-07 03:27:00 -0400

    se envolvio material app en bloc provider para que provea el estado

--- lib/app.dart
+++ lib/app.dart
@@ -1,24 +1,31 @@
+import 'package:app_cart/blocs/cart/cart_bloc.dart';
+import 'package:app_cart/features/cart/presentation/pages/cart_page.dart';
+import 'package:app_cart/features/home/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'features/home/presentation/pages/home_page.dart';
-import 'features/cart/presentation/pages/cart_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'E-Commerce App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
+    // 1. El BlocProvider envuelve a toda la aplicación
+    return BlocProvider<CartBloc>(
+      create: (context) => CartBloc(),
+      // 2. El hijo debe ser el MaterialApp
+      child: MaterialApp(
+        title: 'E-Commerce App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          useMaterial3: true,
+        ),
+        initialRoute: HomePage.route,
+        routes: {
+          HomePage.route: (context) => const HomePage(),
+          CartPage.route: (context) => const CartPage(),
+        },
       ),
-      initialRoute: HomePage.route,
-      routes: {
-        HomePage.route: (context) => const HomePage(),
-        CartPage.route: (context) => const CartPage(),
-      },
     );
   }
 }

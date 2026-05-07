f720f40227591d6eae2d84e0c77018a8c0a52cc9
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-07 02:36:30 -0400

    implementacion de la logica encargada de orquestar los eventos y estados mediante los metodos, se implemento addProduct

---
+++ lib/blocs/cart/cart_bloc.dart
@@ -0,0 +1,40 @@
+import 'package:app_cart/blocs/cart/cart_event.dart';
+import 'package:app_cart/blocs/cart/cart_state.dart';
+import 'package:app_cart/core/domain/cart_item.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CartBloc extends Bloc<CartEvent, CartState> {
+  CartBloc() : super(CartEmpty()) {
+    on<AddProduct>(_onAddProduct);
+    on<RemoveProduct>(_onRemoveProduct);
+  }
+
+  void _onAddProduct(AddProduct event, Emitter<CartState> emit) {
+    final currentState = state;
+    List<CartItem> updatedItems;
+
+    if (currentState is CartUpdated) {
+      updatedItems = List<CartItem>.from(currentState.items);
+      final existingIndex = updatedItems.indexWhere(
+        (item) => item.product.id == event.product.id,
+      );
+
+      if (existingIndex > 0) {
+        final existing = updatedItems[existingIndex];
+        updatedItems[existingIndex] = CartItem(
+          product: event.product,
+          quantity: existing.quantity + event.quantity,
+        );
+      } else {
+        updatedItems.add(
+          CartItem(product: event.product, quantity: event.quantity),
+        );
+      }
+    } else {
+      updatedItems = [
+        CartItem(product: event.product, quantity: event.quantity),
+      ];
+    }
+    emit(CartUpdated(items: updatedItems));
+  }
+}
